165ba50de60376dd494027cc7147965f76def917
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-04-18 12:55:36 -0700

    Async Writes in Redux: WIP

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,26 +2,46 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor (props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors:{}
+    };
   }
 
   render () {
     return (
-      <h1>Hello</h1>
+      <CourseForm
+        allAuthors={this.props.authors}
+        course={this.state.course}
+        errors={this.state.errors}
+      />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => {
+  let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: author.firstName + ' ' + author.lastName
+    };
+  });
+
   return {
-    prop: state.prop
+    course: course,
+    authors: authorsFormattedForDropdown
   };
 };
 
--- src/reducers/authorReducer.js
+++ src/reducers/authorReducer.js
@@ -1,12 +1,12 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function courseReducer(state = initialState.courses, action){
+export default function authorReducer(state = initialState.courses, action){
   switch(action.type) {
-    case types.LOAD_COURSES_SUCCESS:
-      // state.push(action.course);
+    case types.LOAD_AUTHORS_SUCCESS:
+      // state.push(action.author);
       // return state;
-      return action.courses;
+      return action.authors;
     default:
       return state;
   }

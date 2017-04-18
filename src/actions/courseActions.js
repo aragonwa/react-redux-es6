8e8361cd204797c0824e10212917a9ff8159067c
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-04-18 13:37:51 -0700

    Async Writes in Redux: Complete

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,11 +4,27 @@ import courseApi from '../api/mockCourseApi';
 export function loadCoursesSuccess (courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
+export function createCourseSuccess (course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course};
+}
+export function updateCourseSuccess (course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course};
+}
 
 export function loadCourses(){
   return function(dispatch){
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+//getState to get pieces of state from store instead of passing them in
+export function saveCourse(course){
+  return function(dispatch, getState){
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
